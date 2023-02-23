85ced2369b62f1e539b0cffee7ce9893327dc08d
Solitario97 <a.nurmagambetov97@gmail.com>
2023-02-23 07:23:04 +0600

    Go ahead v2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const squareInput = document.querySelector('#square-input');
 const squareRange = document.querySelector('#square-range');
 
+const inputs = document.querySelectorAll('input');
+
+const basePrice = 50000;
+
 squareRange.addEventListener('input', function () {
     squareInput.value = squareRange.value;
 });
@@ -8,3 +12,16 @@ squareRange.addEventListener('input', function () {
 squareInput.addEventListener('input', function () {
     squareRange.value = squareInput.value;
 });
+
+function calculate () {
+    totalPrice = basePrice * parseInt(squareInput);
+    console.log (totalPrice);
+}
+
+for (const input of inputs) {
+    input.addEventListener('input', function () {
+        calculate ();
+    });
+
+
+}

6af78aa943fd2d93b4e5b85a53fcf739e4fca7ba
Solitario97 <a.nurmagambetov97@gmail.com>
2023-02-24 07:10:15 +0600

    litle-big steps v2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const squareInput = document.querySelector('#square-input');
 const squareRange = document.querySelector('#square-range');
-const totalPrice1 = document.querySelector('#total-price');
+const totalPriceElement = document.querySelector('#total-price');
 
-
+const radioType = document.querySelectorAll('input[name="type"]');
 const inputs = document.querySelectorAll('input');
 const basePrice = 50000;
 
@@ -16,8 +16,16 @@ squareInput.addEventListener('input', function () {
 
 function calculate() {
     let totalPrice = basePrice * parseInt(squareInput.value);
-    console.log(totalPrice);
-    totalPrice1.textContent = totalPrice;
+
+    for (const radio of radioType) {
+        if (radio.checked) {
+            totalPrice *= parseFloat(radio.value);
+        }
+    }
+
+    /* приводит формат числа в более удобно-читаемый вид */
+    const formatter = new Intl.NumberFormat('ru');
+    totalPriceElement.textContent = formatter.format(totalPrice);
 }
 
 calculate();  

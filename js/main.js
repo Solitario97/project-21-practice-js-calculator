f72b51fbdfb2263ce284c29304045e59717d7ea7
Solitario97 <a.nurmagambetov97@gmail.com>
2023-02-25 07:17:23 +0600

    final

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 const squareInput = document.querySelector('#square-input');
 const squareRange = document.querySelector('#square-range');
 const totalPriceElement = document.querySelector('#total-price');
-
-const radioType = document.querySelectorAll('input[name="type"]');
 const inputs = document.querySelectorAll('input');
+
+/* Радиокнопки */
+const radioType = document.querySelectorAll('input[name="type"]');
+const radioBuilding = document.querySelectorAll('input[name="building"]');
+const radioRooms = document.querySelectorAll('input[name="rooms"]');
+
+/* Чекбоксы */
+const ceiling = document.querySelector('input[name="ceiling"]');
+const walls = document.querySelector('input[name="walls"]');
+const floor = document.querySelector('input[name="floor"]');
+
+
 const basePrice = 50000;
 
 squareRange.addEventListener('input', function () {
@@ -22,6 +32,31 @@ function calculate() {
             totalPrice *= parseFloat(radio.value);
         }
     }
+    
+    for (const radio of radioBuilding) {
+        if (radio.checked) {
+            totalPrice *= parseFloat(radio.value);
+        }
+    }
+
+    for (const radio of radioRooms) {
+        if (radio.checked) {
+            totalPrice *= parseFloat(radio.value);
+        }
+    }
+
+    if (ceiling.checked) {
+        totalPrice *= parseFloat(ceiling.value);
+    }
+
+    if (walls.checked) {
+        totalPrice *= parseFloat(walls.value);
+    }
+    
+    if (floor.checked) {
+        totalPrice *= parseFloat(floor.value);
+    }
+
 
     /* приводит формат числа в более удобно-читаемый вид */
     const formatter = new Intl.NumberFormat('ru');
